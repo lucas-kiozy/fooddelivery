ad6820b1ca58c87f902bc60c90d0502ecee44ccd
Lucas Vieira <l_vieira@hotmail.com.br>
2025-07-29 18:41:03 -0300

    Primeiro comiit, criar arquivo

---
+++ app.py
@@ -0,0 +1,8 @@
+print('Sabor Express - Sistema de Entrega de Alimentos\n')
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('\nEscolha uma opção: ')
+print('\nVocê escolheu a opção:', opcao_escolhida)
