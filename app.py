3f6eb529b278c40fcede43fc48f06f0ee54cc148
Lucas Vieira <l_vieira@hotmail.com.br>
2025-07-31 23:03:44 -0300

    Encapsulada em funções

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 import os
-print("Sabor Express - Sistema de Entrega de Alimentos")
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair')
 
-opcao_escolhida = int(input('\nEscolha uma opção: '))
-print(f'\nVocê escolheu a opção: {opcao_escolhida}\n' )
-print(type(opcao_escolhida))
+def exibir_nome_do_programa():
+      print("Sabor Express - Sistema de Entrega de Alimentos\n")
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante')
-    nome_restaurante = input('Digite o nome do restaurante: ')
-    print(f'\nRestaurante {nome_restaurante} cadastrado com sucesso!')
-elif opcao_escolhida == 2:
-      print('Listar Restaurantes')
-elif opcao_escolhida == 3:
-      print('Ativar Restaurante')
-      nome_restaurante = input('Digite o nome do restaurante a ser ativado: ')
-      print(f'\nRestaurante {nome_restaurante} ativado com sucesso!')
-else:
+def exibir_menu():
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurantes')
+      print('3. Ativar restaurante')
+      print('4. Sair')
+
+
+def finalizar_programa():
       os.system('cls')
       print('Encerrando o programa. Obrigado por usar o Sabor Express!')
       print('...\n')
+
+def escolher_opcao():
+      opcao_escolhida = int(input('\nEscolha uma opção: '))
+      print(f'\nVocê escolheu a opção: {opcao_escolhida}\n' )
+      if opcao_escolhida == 1:
+            print('Cadastrar Restaurante')
+            nome_restaurante = input('Digite o nome do restaurante: ')
+            print(f'\nRestaurante {nome_restaurante} cadastrado com sucesso!')
+      elif opcao_escolhida == 2:
+            print('Listar Restaurantes')
+      elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+            nome_restaurante = input('Digite o nome do restaurante a ser ativado: ')
+            print(f'\nRestaurante {nome_restaurante} ativado com sucesso!')
+      elif opcao_escolhida == 4:
+            finalizar_programa()
+      else:
+            print('Opção inválida. Encerrando o programa.')
+            finalizar_programa()
+
+def main():
+      exibir_nome_do_programa()
+      exibir_menu()
+      escolher_opcao()
+
+if __name__ == "__main__":
+      main()
